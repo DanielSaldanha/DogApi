48d8146b4989f02cce51509a968a8c5ff6887dd5
DanielSaldanha <danielpedrosaldanha@gmail.com>
2025-04-30 09:27:35 -0300

    Adicionar arquivos de projeto.

---
+++ DogApi/Controllers/DogController.cs
@@ -0,0 +1,32 @@
+﻿using DogApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DogController : ControllerBase
+    {
+        private readonly IHttpClientFactory _IHttpClientFactory;
+        public DogController(IHttpClientFactory ihttpclientfactory)
+        {
+            _IHttpClientFactory = ihttpclientfactory;
+        }
+        [HttpGet("pegue um cão")]
+        public async Task<ActionResult> DogerGet()
+        {
+            var client = _IHttpClientFactory.CreateClient("CurrencyAPI");
+            var response = await client.GetAsync($"breeds/list/all");
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound("nao funcionou");
+            }
+            var DogJson = await response.Content.ReadFromJsonAsync<DogModel>();
+            if(DogJson == null)
+            {
+                return NotFound("sem chances");
+            }
+            return Ok(DogJson);
+        }
+    }
+}
---
+++ DogApi/Model/DogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DogApi.Model
+{
+    public class DogModel
+    {
+        public Dictionary<string, List<string>>? Message { get; set; }
+        public string? Status { get; set; }
+        // public string? breed { get; set; }
+    }
+}
